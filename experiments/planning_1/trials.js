100b9777f5e882cb3e8b56d3a6809c80a6a3fed2
Alicia Chen <amqchen@gmail.com>
2025-09-28 19:46:02 -0400

    small edit to instructions for prev experiments

--- experiments/planning_1/trials.js
+++ experiments/planning_1/trials.js
@@ -37,7 +37,7 @@ export function makeTimeline(
                 <h2>Social interactions survey</h2>
                 <p>In this survey, you will read vignettes about two people in different kinds of social relationships, sharing different kinds of food in different situations. For each scenario, you will read about four different actions the two people can take.</p>
                 <p>For each action, we will ask you to evaluate the likelihood of the two people choosing that action. Please consider each option independently.</p>
-                <p>Please read each of the scenarios and ways of sharing food carefully! 🙂 You will receive $5 if you successfully complete the survey. </p>
+                <p>Please pay attention to the social relationship between the two people, and read each of the scenarios and ways of sharing food carefully! 🙂 You will receive $5 if you successfully complete the survey. </p>
                 <p>Please do not close the window until you have completed the survey. If you do so, you will lose your progress.</p>
                 <p>Press next to begin the survey.</p>
             </div>
